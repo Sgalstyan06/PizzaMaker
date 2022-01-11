6e59676da4500e9353fee09eb9ff07e9b50a6943
Galstyan Suren <galstyan.suren.1986@gmail.com>
2022-01-11 11:53:19 +0400

    Barev Sensey

---
+++ src/main/java/com/pizza/mypizzaproject/controller/TableController.java
@@ -0,0 +1,4 @@
+package com.pizza.mypizzaproject.controller;
+
+public class TableController {
+}
---
+++ src/main/java/com/pizza/mypizzaproject/service/impl/TableServiceImpl.java
@@ -0,0 +1,6 @@
+package com.pizza.mypizzaproject.service.impl;
+
+import com.pizza.mypizzaproject.service.TableService;
+
+public class TableServiceImpl implements TableService {
+}
